31f73b37bffb6b0e1dc62678d3e4b52ea76d7f2a
dolev damri <dolevd2012@gmail.com>
2021-05-31 08:48:38 -0600

    Added Hard Difficulty

--- memorygame/CollectionViewController.swift
+++ memorygame/CollectionViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 private let reuseIdentifier = "Cell"
 
-var array: [String] = ["pig","pig","snake","snake","cat","cat","eagle","eagle","sheep","sheep","zebra","zebra","hyena","hyena","rabbit","rabbit"]
+var array: [String] = ["pig","pig","snake","snake","cat","cat","eagle","eagle","sheep","sheep","zebra","zebra","hyena","hyena","rabbit","rabbit","penguin","penguin","panda","panda"]
 var cardsArray: [Card] = []
 var firstCard = Card.init(animalName:  "EmptyFirstCard")
 var firstCardCellIndex: IndexPath!
@@ -18,21 +18,27 @@ var secondCard = Card.init(animalName: "")
 var count : Int = 0
 var sectionFooterr: SectionFooter? = nil
 var timer : Int = 0
+var difficulty :String!
 class CollectionViewController: UICollectionViewController {
 var timerInterval:Timer?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        difficulty = UserDefaults.standard.string(forKey: "Difficulty")
+        if (difficulty == "Normal"){
+            array = ["pig","pig","snake","snake","cat","cat","eagle","eagle","sheep","sheep","zebra","zebra","hyena","hyena","rabbit","rabbit"]
+        }
         for i in 0..<array.count{
             cardsArray.append(Card.init(animalName: array[i]))
         }
         cardsArray.shuffle()
-        
+    
 
     }
+//MARK: number of cells in the page
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return cardsArray.count
     }
+//MARK: create a custom cell for all cells
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         var cell = UICollectionViewCell()
@@ -46,6 +52,7 @@ var timerInterval:Timer?
         
         return cell
     }
+//MARK: adding the footer to the UICollectionView
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
             
             if let sectionFooter = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "FooterView", for: indexPath)as? SectionFooter{
@@ -59,7 +66,7 @@ var timerInterval:Timer?
         /// Normally should never get here
         return UICollectionReusableView()
     }
-    
+//MARK: adding click event for each cell and adding logic
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
         let currentcell = collectionView.cellForItem(at: indexPath)as! CollectionViewCell
@@ -113,12 +120,13 @@ var timerInterval:Timer?
         }
         
     }
+//MARK: Adding end logic to the game
     func checkGameOver() -> Bool{
         var c : Int = 0
         for i in 0..<cardsArray.count{
             if(cardsArray[i].flipped == true){
                 c = c + 1
-                if(c == 16){
+                if(c == cardsArray.count){
                     timerInterval?.invalidate()
                    return true
                 }
@@ -127,11 +135,22 @@ var timerInterval:Timer?
         }
         return false
     }
+//MARK: Thread that count seconds
     @objc func action(){
         sectionFooterr!.timer.text = "timer: \(timer+1)"
         timer = timer + 1
         
     }
-
+    class sharedPrefValue : Codable{
+        var difficulty : String
+        var roundsTaken : Int
+        
+        
+        init(difficulty: String, rounds: Int){
+            self.difficulty = difficulty
+            self.roundsTaken = rounds
+        }
+        
+    }
     
 }
--- memorygame/FirstController.swift
+++ memorygame/FirstController.swift
@@ -9,23 +9,34 @@ import UIKit
 
 class FirstController: UIViewController {
 
-    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var playNormalButton: UIButton!
+    @IBOutlet weak var playHardButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        playButton.addTarget(self, action: #selector(playButtonClicked), for: .touchUpInside)
 
     }
     
-    @objc func playButtonClicked() {
+
+    @IBAction func NormalButtonClicked(_ sender: Any) {
+        UserDefaults.standard.setValue("Normal", forKey: "Difficulty")
         let story = UIStoryboard(name: "Main", bundle: nil)
         let controller = story.instantiateViewController(identifier: "CollectionViewController")as! CollectionViewController
         let navigation = UINavigationController(rootViewController: controller)
         self.view.addSubview(navigation.view)
         self.addChild(navigation)
         navigation.didMove(toParent: self)
-        
     }
     
-
+    @IBAction func HardButtonClicked(_ sender: Any) {
+        UserDefaults.standard.setValue("Hard", forKey: "Difficulty")
+        let story = UIStoryboard(name: "Main", bundle: nil)
+        let controller = story.instantiateViewController(identifier: "CollectionViewController")as! CollectionViewController
+        let navigation = UINavigationController(rootViewController: controller)
+        self.view.addSubview(navigation.view)
+        self.addChild(navigation)
+        navigation.didMove(toParent: self)
+    }
+    
+    
 
 }
